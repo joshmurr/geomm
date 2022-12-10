5220e4b9d0455ae4d4dd44ed48aaa85da28c4f11
joshmurr <joshuadavidmurr@gmail.com>
2022-12-10 02:31:44 +0000

    Initial setup.. took a while

---
+++ packages/webgl/lib/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@geomm/core");
+(0, core_1.hello)('hi');
+//# sourceMappingURL=index.js.map
---
+++ packages/webgl/src/index.ts
@@ -0,0 +1,3 @@
+import { hello } from '@geomm/core'
+
+hello('hi')
