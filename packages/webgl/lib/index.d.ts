07316656f9c4fbbb4d60ec33aca2b44e3cdf65bb
joshmurr <joshuadavidmurr@gmail.com>
2022-12-19 12:12:09 +0000

    Stop watching lib

--- packages/webgl/lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-export * from './uniforms';
-export * from './shaders';
-export * from './dom';
-export * from './buffers';
-export * from './primitives';
-export * from './textures';
-export * from './setters';
-//# sourceMappingURL=index.d.ts.map
--- packages/webgl/lib/index.js
+++
@@ -1,7 +0,0 @@
-export * from './uniforms';
-export * from './shaders';
-export * from './dom';
-export * from './buffers';
-export * from './primitives';
-export * from './textures';
-export * from './setters';
