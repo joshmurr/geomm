bca15caf761979f9ee662c248026c12731a83026
Josh Murr <joshuadavidmurr@gmail.com>
2023-05-04 15:31:00 +0100

    Create element from string

--- packages/dom/src/core.ts
+++ packages/dom/src/core.ts
@@ -2,6 +2,8 @@ export const createEl = <T>(type: string): T => {
   return document.createElement(type) as T
 }
 
-export const add = (el: HTMLElement) => {
-  document.body.appendChild(el)
+export const add = (el: Node | string) => {
+  const element = typeof el === 'string' ? createEl<Node>(el) : el
+  document.body.appendChild(element)
+  return element
 }
