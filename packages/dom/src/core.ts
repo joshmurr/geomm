f9fa2fa6c69705616dc376d0bd0aba65fdb747da
Josh Murr <joshuadavidmurr@gmail.com>
2023-05-12 12:11:17 +0100

    createEl with opts

--- packages/dom/src/core.ts
+++ packages/dom/src/core.ts
@@ -1,9 +1,31 @@
-export const createEl = <T>(type: string): T => {
-  return document.createElement(type) as T
+const isEventListener = (key: string) => key.startsWith('on')
+
+export const createEl = <T extends HTMLElement>(
+  type: string,
+  opts?: {
+    [key: string]: number | string | EventListenerOrEventListenerObject
+  },
+): T => {
+  const el = document.createElement(type) as T
+  if (opts) {
+    Object.keys(opts).forEach((key) => {
+      if (isEventListener(key)) {
+        el.addEventListener(
+          key.slice(2).toLowerCase(),
+          opts[key] as EventListenerOrEventListenerObject,
+        )
+        return
+      }
+
+      el.setAttribute(key, opts[key].toString())
+    })
+  }
+
+  return el
 }
 
-export const add = (el: Node | string) => {
-  const element = typeof el === 'string' ? createEl<Node>(el) : el
+export const add = (el: HTMLElement | string) => {
+  const element = typeof el === 'string' ? createEl<HTMLElement>(el) : el
   document.body.appendChild(element)
   return element
 }
