e9779dec9b3408a065ad06b5edf62b77dc34e0b7
Josh Murr <joshuadavidmurr@gmail.com>
2023-05-18 18:09:49 +0100

    Use another canvas as input

--- examples/chladni-cpu-image/src/index.ts
+++ examples/chladni-cpu-image/src/index.ts
@@ -38,6 +38,21 @@ const imageToGrayscaleArray = (img: HTMLImageElement) => {
   return grayscale
 }
 
+const ctxToGrayscaleArray = (ctx: CanvasRenderingContext2D) => {
+  const imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height)
+  const data = imageData.data
+  const grayscale = []
+  for (let i = 0; i < data.length; i += 4) {
+    const r = data[i]
+    const g = data[i + 1]
+    const b = data[i + 2]
+    const avg = (r + g + b) / 3 / 255
+    const rescaled = avg // === 0 ? 0 : avg * 2 - 1
+    grayscale.push(rescaled)
+  }
+  return grayscale
+}
+
 seedImage.onload = () => {
   const grayscale = imageToGrayscaleArray(seedImage)
   init(grayscale)
@@ -47,8 +62,7 @@ export type Settings = {
   [key: string]: Setting
 }
 
-let size = [512, 512] as const
-let aspect = size[0] / size[1]
+const size = vec(512, 512)
 
 const settings: Settings = {
   nParticles: {
@@ -63,7 +77,7 @@ const settings: Settings = {
   b: { val: 1.2, min: 0.1, max: 10 },
   m: { val: 8, min: 0.1, max: 10 },
   n: { val: 4, min: 0.1, max: 10 },
-  vel: { val: 0.03, min: 0.0001, max: 0.1 },
+  vel: { val: 0.003, min: 0.0001, max: 0.1 },
 }
 
 const bound = (p: Particle) => {
@@ -72,27 +86,17 @@ const bound = (p: Particle) => {
   if (p.pos.y < 0) p.pos.y = 0
   if (p.pos.y > 1) p.pos.y = 1
 }
+const move = (p: Particle, seed: number[] | CanvasRenderingContext2D) => {
+  const { vel, minWalk } = settings
 
-const chladni = (v: Vec, a: number, b: number, m: number, n: number) =>
-  /* chladni 2D closed-form solution - returns between -1 and 1 */
-  a * sin(PI * n * v.x * aspect) * sin(PI * m * v.y) +
-  b * sin(PI * m * v.x * aspect) * sin(PI * n * v.y)
+  const pos = toScreen(p.pos, size)
+  const eq = Array.isArray(seed)
+    ? seed[Math.floor(pos.x) + Math.floor(pos.y) * size.x]
+    : getPixelFromCtx(seed, pos)
 
-const move = (p: Particle, seed: number[]) => {
-  // what is our chladni value i.e. how much are we vibrating? (between -1 and 1, zeroes are nodes)
-  const { a, b, m, n, vel, minWalk } = settings
-  /* const eq = chladni(p.pos, a.val, b.val, m.val, n.val) */
-
-  const pos = toScreen(p.pos, vec(...size))
-  const idx = Math.floor(pos.x) + Math.floor(pos.y) * size[0]
-  const eq = seed[idx]
-
-  // set the amplitude of the move -> proportional to the vibration
   p.stochasticAmplitude = vel.val * abs(eq)
-
   if (p.stochasticAmplitude <= minWalk.val) p.stochasticAmplitude = minWalk.val
 
-  // perform one random walk
   p.pos.x += randRange(
     -p.stochasticAmplitude * 0.5,
     p.stochasticAmplitude * 0.5,
@@ -105,27 +109,63 @@ const move = (p: Particle, seed: number[]) => {
   bound(p)
 }
 
+const drawRadialGradient = (
+  ctx: CanvasRenderingContext2D,
+  center: Vec,
+  radius: number,
+  color: string,
+) => {
+  const gradient = ctx.createRadialGradient(
+    center.x,
+    center.y,
+    0,
+    center.x,
+    center.y,
+    radius,
+  )
+  gradient.addColorStop(0, color)
+  gradient.addColorStop(1, 'transparent')
+  ctx.fillStyle = gradient
+  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+}
+
+let MOUSE = vec(0, 0)
+
+const clearCtx = (ctx: CanvasRenderingContext2D) => {
+  ctx.fillStyle = 'rgba(0,0,0,0.8)'
+  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+}
+
+const getPixelFromCtx = (ctx: CanvasRenderingContext2D, pos: Vec) => {
+  const imageData = ctx.getImageData(pos.x, pos.y, 1, 1)
+  const data = imageData.data
+  return data[0] + data[1] + data[2] / 255 / 3
+}
+
+const drawParticle = (p: Particle, ctx: CanvasRenderingContext2D) => {
+  const { x, y } = toScreen(p.pos, size)
+  const { stochasticAmplitude: sa } = p
+  ctx.fillStyle = `hsl(${sa * 720 * 2}, 100%, 50%)`
+  ctx.fillRect(x, y, 2, 2)
+}
+
 const init = (seed: number[]) => {
-  const c = canvas(...size)
+  const c = canvas(size.x, size.y)
   appendEl(c)
 
-  const gui = makeGui(settings)
+  const testCanvas = canvas(size.x, size.y)
+  appendEl(testCanvas)
 
-  console.log(seed)
+  const testCtx = testCanvas.getContext('2d') as CanvasRenderingContext2D
+  testCtx.fillStyle = 'black'
+  testCtx.fillRect(0, 0, testCanvas.width, testCanvas.height)
 
-  window.addEventListener('resize', () => {
-    size = [window.innerWidth, window.innerHeight] as const
-    aspect = size[0] / size[1]
-    c.width = window.innerWidth
-    c.height = window.innerHeight
-  })
+  window.addEventListener(
+    'mousemove',
+    (e) => (MOUSE = vec(e.offsetX, e.offsetY)),
+  )
 
-  const drawParticle = (p: Particle, ctx: CanvasRenderingContext2D) => {
-    const { x, y } = toScreen(p.pos, vec(c.width, c.height))
-    const { stochasticAmplitude: sa } = p
-    ctx.fillStyle = `hsl(${sa * 720 * 2}, 100%, 50%)`
-    ctx.fillRect(x, y, 2, 2)
-  }
+  /* const gui = makeGui(settings) */
 
   const particles = Array.from(
     { length: settings.nParticles.max as number },
@@ -136,14 +176,20 @@ const init = (seed: number[]) => {
   )
 
   const draw = (particles: Particle[], ctx: CanvasRenderingContext2D) => {
+    /* clearCtx(testCtx) */
+    /* drawRadialGradient(testCtx, MOUSE, 100, 'white') */
+    /**/
+    clearCtx(ctx)
     const slice = particles.slice(0, settings.nParticles.val)
     slice.forEach((p) => move(p, seed))
-    ctx.fillStyle = 'black'
-    ctx.fillRect(0, 0, c.width, c.height)
     slice.forEach((p) => drawParticle(p, ctx))
 
-    requestAnimationFrame(() => draw(slice, ctx))
+    requestAnimationFrame(() => draw(particles, ctx))
   }
 
   draw(particles, c.getContext('2d') as CanvasRenderingContext2D)
+
+  window.addEventListener('click', (e) =>
+    draw(particles, c.getContext('2d') as CanvasRenderingContext2D),
+  )
 }
