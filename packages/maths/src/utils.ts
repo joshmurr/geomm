58adc8baf43cec9e636f7d1c84b2816c261f95bc
Josh Murr <joshuadavidmurr@gmail.com>
2023-06-29 08:55:45 +0100

    Clamp

--- packages/maths/src/utils.ts
+++ packages/maths/src/utils.ts
@@ -1,3 +1,5 @@
 export const ratio = (a: number, b: number) => a / b
 export const aspect = (w: number, h: number) => ratio(w, h)
 export const isPowerOf2 = (value: number) => (value & (value - 1)) === 0
+export const clamp = (value: number, min: number, max: number) =>
+  Math.min(Math.max(value, min), max)
