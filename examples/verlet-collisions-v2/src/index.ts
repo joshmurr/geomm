779248adc0390dc154826da671f4171bf97467dc
Josh Murr <joshuadavidmurr@gmail.com>
2023-09-24 11:27:38 +0100

    Simple grid space partitioning does improve things a lot

--- examples/verlet-collisions-v2/src/index.ts
+++ examples/verlet-collisions-v2/src/index.ts
@@ -2,8 +2,15 @@
  * https://web.archive.org/web/20191117195626/http://codeflow.org/entries/2010/nov/29/verlet-collision-with-impulse-preservation/
  */
 import { appendEl, createEl } from '@geomm/dom'
-import { add, distance, mag, scale, sub, Vec, vec } from '@geomm/geometry'
+import { add, distance, mag, mul, scale, sub, Vec, vec } from '@geomm/geometry'
 import { randInt, randRange, sqrt } from '@geomm/maths'
+import {
+  drawGrid,
+  drawGridCell,
+  drawGridNumbered,
+  getCellPos,
+  getHashBucketIndex,
+} from './grid'
 
 type Particle = {
   pos: Vec
@@ -14,11 +21,18 @@ type Particle = {
 }
 
 const settings = {
-  N_PARTICLES: 512,
+  N_PARTICLES: 1,
+  RADIUS: 8,
 }
 
-const SIZE = vec(300, 600)
+const SIZE = vec(1000, 1000)
 const GRAVITY = vec(0, 0.01)
+
+const CELL_SIZE = settings.RADIUS
+
+const N_BUCKETS = ((SIZE.x / CELL_SIZE) * SIZE.y) / CELL_SIZE
+
+console.log('BUCKETS', N_BUCKETS)
 
 const c = createEl('canvas', {
   width: SIZE.x,
@@ -29,35 +43,55 @@ appendEl(c)
 
 const particles: Particle[] = []
 
-while (particles.length < settings.N_PARTICLES) {
-  const mass = randInt(5, 8)
-  const initialPos = vec(
-    randRange(mass, SIZE.x - mass),
-    randRange(mass, SIZE.y - mass),
-  )
-  const col = '#fff'
-  const p = {
-    pos: initialPos,
-    prevPos: initialPos,
+const useMyParticles = () => {
+  particles.push({
+    pos: vec(100, 100),
+    prevPos: vec(100, 100),
     acc: vec(0, 0),
-    mass,
-    col,
-  }
+    mass: settings.RADIUS,
+    col: '#fff',
+  })
 
-  let collides = false
-  for (let i = 0; i < particles.length; i++) {
-    const p2 = particles[i]
-    const { pos: pos2, mass: mass2 } = p2
-    const v = sub(p.pos, pos2)
-    const dist = mag(v)
-    const minDist = mass + mass2
+  particles.push({
+    pos: vec(200, 200),
+    prevPos: vec(200, 200),
+    acc: vec(0, 0),
+    mass: settings.RADIUS,
+    col: '#fff',
+  })
+}
 
-    if (dist < minDist) {
-      collides = true
-      break
+const placeParticles = () => {
+  while (particles.length < settings.N_PARTICLES) {
+    const mass = settings.RADIUS // randInt(5, 8)
+    const initialPos = vec(
+      randRange(mass, SIZE.x - mass),
+      randRange(mass, SIZE.y - mass),
+    )
+    const col = '#fff'
+    const p = {
+      pos: initialPos,
+      prevPos: initialPos,
+      acc: vec(0, 0),
+      mass,
+      col,
     }
+
+    let collides = false
+    for (let i = 0; i < particles.length; i++) {
+      const p2 = particles[i]
+      const { pos: pos2, mass: mass2 } = p2
+      const v = sub(p.pos, pos2)
+      const dist = mag(v)
+      const minDist = mass + mass2
+
+      if (dist < minDist) {
+        collides = true
+        break
+      }
+    }
+    if (!collides) particles.push(p)
   }
-  if (!collides) particles.push(p)
 }
 
 const accelerate = (p: Particle, dt: number) => {
@@ -80,9 +114,12 @@ const applyForce = (p: Particle, force: Vec) => {
 }
 
 const simpleConstrain = (p: Particle) => {
-  const { pos, mass } = p
+  const { pos, prevPos, mass } = p
+  const diff = sub(pos, prevPos)
   if (pos.x < mass) {
     pos.x = mass
+    // this.old_x = this.x + vel_x;
+    prevPos.x = pos.x + diff.x
   } else if (pos.x > SIZE.x - mass) {
     pos.x = SIZE.x - mass
   }
@@ -93,7 +130,7 @@ const simpleConstrain = (p: Particle) => {
   }
 }
 
-const checkCollisions = (p: Particle, idx: number) => {
+const checkCollisions = (p: Particle, idx: number, preserveImpulse = false) => {
   const { pos, mass } = p
 
   for (let i = idx + 1; i < particles.length; i++) {
@@ -111,19 +148,87 @@ const checkCollisions = (p: Particle, idx: number) => {
 
       p.pos = sub(p.pos, displacement)
       p2.pos = add(p2.pos, displacement)
+
+      /* if(preserveImpulse) { */
+      /*   const damping = 0.5 */
+      /*   const { prevPos } = p */
+      /*   const { prevPos: prevPos2 } = p2 */
+      /*   const v1 = sub(pos, prevPos) */
+      /*   const v2 = sub(pos2, prevPos2) */
+      /*   const f1 = scale(scale(mul(v, v1), damping), 1 / distSq) */
+      /*   const f2 = scale(scale(mul(v, v2), damping), 1 / distSq) */
+      /**/
+      /* } */
     }
   }
 }
 
-const steps = 4
-const dt = 1
+const vecId = (v: Vec) => `${v.x},${v.y}`
+
+const checkCollisionsGrid = (particles: Particle[]) => {
+  const grid = new Map()
+  particles.forEach((p) => {
+    const { pos } = p
+    const cellPosTopLeft = getCellPos(
+      add(pos, vec(-settings.RADIUS, -settings.RADIUS)),
+      CELL_SIZE,
+    )
+    const cellPosBottomRight = getCellPos(
+      add(pos, vec(settings.RADIUS, settings.RADIUS)),
+      CELL_SIZE,
+    )
+
+    for (let i = 0; i <= cellPosBottomRight.x - cellPosTopLeft.x; i++) {
+      for (let j = 0; j <= cellPosBottomRight.y - cellPosTopLeft.y; j++) {
+        const cellPos = vec(cellPosTopLeft.x + i, cellPosTopLeft.y + j)
+        const idx = vecId(cellPos)
+        if (!grid.has(idx)) grid.set(idx, [])
+        const cell = grid.get(idx)
+        cell.push(p)
+      }
+    }
+  })
+
+  for (const bucket of grid.values()) {
+    for (let i = 0; i < bucket.length; i++) {
+      const p = bucket[i]
+      const { pos, mass } = p
+      for (let j = i + 1; j < bucket.length; j++) {
+        const p2 = bucket[j]
+        const { pos: pos2, mass: mass2 } = p2
+        const v = sub(pos, pos2)
+        const distSq = v.x * v.x + v.y * v.y
+        const minDist = mass + mass2
+
+        if (distSq < minDist * minDist) {
+          const dist = sqrt(distSq)
+          const factor = (dist - minDist) / dist
+
+          const displacement = scale(v, factor * 0.5)
+
+          p.pos = sub(p.pos, displacement)
+          p2.pos = add(p2.pos, displacement)
+        }
+      }
+    }
+  }
+}
+
+const filterStrength = 20
+let lastLoop = performance.now()
+let thisLoop = lastLoop
+let frameTime = 0
+
+const steps = 1
+const dt = 1 / steps
 const step = () => {
   ctx.clearRect(0, 0, SIZE.x, SIZE.y)
 
   for (let sub = 0; sub < steps; sub++) {
     particles.forEach((p) => applyForce(p, GRAVITY))
-    particles.forEach((p) => accelerate(p, dt / steps))
-    particles.forEach((p, i) => checkCollisions(p, i))
+    particles.forEach((p) => accelerate(p, dt))
+    /* particles.forEach((p, i) => checkCollisions(p, i)) */
+    checkCollisionsGrid(particles)
     particles.forEach((p) => simpleConstrain(p))
     particles.forEach((p) => inertia(p))
   }
@@ -137,10 +242,33 @@ const step = () => {
     ctx.beginPath()
     ctx.arc(pos.x, pos.y, mass, 0, Math.PI * 2)
     ctx.fill()
+
+    /* ctx.fillStyle = '#fff' */
+    /* ctx.beginPath() */
+    /* ctx.arc(pos.x, pos.y, 3, 0, Math.PI * 2) */
+    /* ctx.fill() */
   })
+
+  /* drawGridNumbered(ctx, CELL_SIZE, SIZE.x, SIZE.y) */
+
+  thisLoop = performance.now()
+  const thisFrameTime = thisLoop - lastLoop
+  frameTime += (thisFrameTime - frameTime) / filterStrength
+  lastLoop = thisLoop
 
   requestAnimationFrame(step)
 }
 
-/* window.addEventListener('click', () => requestAnimationFrame(step)) */
+const fpsOut = document.getElementById('fps') as HTMLPreElement
+setInterval(function() {
+  fpsOut.innerHTML = `n particles: ${settings.N_PARTICLES}
+${(1000 / frameTime).toFixed(1)}fps`
+}, 1000)
+
+placeParticles()
+/* useMyParticles() */
+
+window.addEventListener('click', () => {
+  for (let i = 0; i < 5; i++) requestAnimationFrame(step)
+})
 requestAnimationFrame(step)
